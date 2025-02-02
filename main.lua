bf7b03127772ea7d987be31ad777ae0107f033c0
Jogla <Joglacraft@gmail.com>
2025-02-02 02:02:41 +0100

    Added Joker alternating,

--- main.lua
+++ main.lua
@@ -15,48 +15,74 @@ for k, v in pairs(mod_contents) do -- Load this mod's files
 	assert(SMODS.load_file('/content/'..v..'.lua'))()
   end
 
--- 
--- NOT Flipped Script im SLOW 
---
+
+--[[
+key is the provided card key.
+table is the reference table to look up.
+passing key or value, and returns the other.
+]] 
+function get_equivalent(key,table,passing)
+	if passing == ("k" or "K" or "Key" or "key") then -- passing key, returning value
+		for k,v in pairs(table) do
+			if k == key then return v end
+		end
+	elseif passing == ("v" or "V" or "Value" or "value") then -- passing value, returning key
+		for k,v in pairs(table) do
+			if v == key then return k end
+		end
+	end
+end
+
+-- Alternates between this card and the associated alternative card.
+function alternate_card(card,table,passing) 
+	local convert = get_equivalent(card.key,table,passing)
+	local card = create_card('Joker', G.jokers, false, nil, true, false, convert, nil)
+	card:add_to_deck()
+	G.jokers:emplace(card)
+end
+-- Missing: Delete original card, transfer all enhancements, editions, etc to the new card.
+
+
+
+local joker_equivalents = {
+	["j_fg_t1"] = "j_fg_t2"
+}
+
+
 SMODS.Joker {
-	key = 'NOTflippedscript',
+	key = 't1',
 	loc_txt = {
-		name = 'NOTFlipped Script',
+		name = 'Test 1',
 		text = {
-			"When sold, changes the edition of",
-			"all other jokers to the opposite edition."
+			"Should become Test 2 when sold"
 		}
 	},
 	config = { extra = {} },
 	rarity = 1,
 	atlas = 'jokers',
 	pos = { x = 1, y = 0 },
-	cost = 20,
+	cost = 0,
 	calculate = function(self, card, context)
 		if context.selling_self then
-			G.E_MANAGER:add_event(Event({
-				func = function()
-					
-					sendInfoMessage("Should be changing if i did it right", "MyInfoLogger")
-					for i in ipairs(G.jokers.cards) do
-						local currentCard = G.jokers.cards[i]
-						-- this kinda jank but it works (i couldnt figure out how to do this with an array :( )
-						if currentCard.edition then
-							if currentCard.edition.negative then
-								currentCard:set_edition("e_polychrome", true)
-							elseif currentCard.edition.polychrome then
-								currentCard:set_edition("e_negative", true)
-							elseif currentCard.edition.holo then
-								currentCard:set_edition("e_foil", true)
-							elseif currentCard.edition.foil then
-								currentCard:set_edition("e_holo", true)
-							end							
-						end
-						-- if wanting to add an edition to cards without one just add it as an else to the g.joker.cards[i].edition
-					end
-					return true
-				end
-			}))
+			alternate_card(self,joker_equivalents,"k")
+		end
+	end
+}
+
+SMODS.Joker {
+	key = 't2',
+	loc_txt = {
+		name = 'Test 2',
+		text = {
+			"Should become Test 1 when sold"
+		}
+	},
+	config = { extra = {} },
+	rarity = 1,
+	cost = 0,
+	calculate = function(self, card, context)
+		if context.selling_self then
+			alternate_card(self,joker_equivalents,"v")
 		end
 	end
 }
