2379ad398c885d862b7c0fb83c64ea0e732d6893
Jogla <Joglacraft@gmail.com>
2025-02-09 13:24:32 +0100

    removed a print so the game doesn't implode

--- content/aux_functions.lua
+++ content/aux_functions.lua
@@ -29,26 +29,13 @@ end
 
 -- Alternates between this card and the associated alternative card.
 -- key is the provided card key.
--- table is the reference table to look up.
+-- card is the card instance you are dealing with. 99% of the time it will be just card (the one provided you by the function)
+-- table is the reference table to look up and compare.
 function alternate_card(key,card,table) 
 	local convert = get_equivalent(key,table,is_alternate(key,table))
 	local new_card = create_card('Joker', G.jokers, false, nil, true, false, convert, nil)
 	new_card:add_to_deck()
 	G.jokers:emplace(new_card)
-	print("New card generated")
-	--local card_edition = card:get_edition()
-	--[[if card_edition ~= nil then
-		for k,v in pairs(card_edition) do
-			print(tostring(k).." | "..tostring(v))
-			card_edition = k
-			if k == "chip_mod" then new_card:set_edition({foil = true},true,true)
-			elseif k == "mult_mod" then new_card:set_edition({holo = true},true,true)
-			elseif k == "x_mult_mod" then new_card:set_edition({polychrone = true},true,true) end
-			break
-		end
-	end
-	]]
-	print(card.edition.key)
 	if card.edition and card.edition.key == "e_foil" then new_card:set_edition({foil = true},true,true)
 	elseif card.edition and card.edition.key == "e_holo" then new_card:set_edition({holo = true},true,true)
 	elseif card.edition and card.edition.key == "e_polychrome" then new_card:set_edition({polychrome = true},true,true)
@@ -58,4 +45,3 @@ function alternate_card(key,card,table)
 	end
 	card:start_dissolve(nil,false,0,true)
 end
--- Missing: Delete original card, transfer all enhancements, editions, etc to the new card.
