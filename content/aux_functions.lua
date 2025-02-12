f4859f7256e38ae912e92da0e7672fdb2f6253e9
Jogla <89071998+Joglacraft@users.noreply.github.com>
2025-02-12 20:10:38 +0100

    Crash hotfix (#5)
    * Update foolsGambit.json
    
    Added own name and changed prefix
    
    * Added my "Jogla" name the mod's credits. Prepared stuff for multiple files.
    
    * Added Joker alternating,
    
    * Moved code around. Functionality remains the same.
    
    * - Fully implemented (for jokers only) card alternance.
    - Changed key for polished edition from 'Polished' to 'polished'.
    - Added Balatro's 1x and 2x joker spritesheet to be used with our custom jokers.
    
    * removed a print so the game doesn't implode
    
    * Hotfix because of the 'boosterpack' name being outdated.
    
    * Adde a line to prevent alternating cards from randomly getting editions.
    
    ---------
    
    Co-authored-by: jenku <80538856+jenkuuuuuu@users.noreply.github.com>

--- content/aux_functions.lua
+++ content/aux_functions.lua
@@ -36,12 +36,10 @@ function alternate_card(key,card,table)
 	local new_card = create_card('Joker', G.jokers, false, nil, true, false, convert, nil)
 	new_card:add_to_deck()
 	G.jokers:emplace(new_card)
-	if card.edition and card.edition.key == "e_foil" then new_card:set_edition({foil = true},true,true)
-	elseif card.edition and card.edition.key == "e_holo" then new_card:set_edition({holo = true},true,true)
-	elseif card.edition and card.edition.key == "e_polychrome" then new_card:set_edition({polychrome = true},true,true)
-	elseif card.edition and card.edition.key == "e_negative" then new_card:set_edition({negative = true},true,true)
-	elseif card.edition and card.edition.key == "e_fg_polished" then new_card:set_edition("e_fg_polished",true,true)
-	else new_card:set_edition(nil,true,true)
+	if card.edition then
+		new_card:set_edition(tostring(card.edition.key),true,true)
+	else
+		new_card:set_edition(nil,true,true)
 	end
 	card:start_dissolve(nil,false,0,true)
 end
