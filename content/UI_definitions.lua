bd4b6a72d30a9d4d2bb87a028026638d3278e3af
Jogla <Joglacraft@gmail.com>
2025-07-14 15:25:03 +0200

    Added custom SFX toggle + revamped config menu layout.

--- content/UI_definitions.lua
+++ content/UI_definitions.lua
@@ -5,83 +5,111 @@ SMODS.current_mod.config_tab = function()
 		r = 0.1, minw = 10, minh = 6, align = "tm", padding = 0.2, colour = G.C.BLACK
 	}, nodes = {
 		-- work your UI wizardry here, see 'Building a UI' page
-		{n = G.UIT.R, config = {minw = 4, minh = 6, align = "tm", padding = 0.1}, nodes = {
-			{n = G.UIT.R, nodes = {
-				{n = G.UIT.C, nodes = {
-					{n = G.UIT.R, nodes = {
-						{n = G.UIT.C, nodes = {
-							create_toggle{
-								label = "Additional jokers",
-								ref_table = FG.config,
-								ref_value = "extra_jokers"
-							},
-							{n = G.UIT.R, config = {align = "tm"}, nodes = {
-								{n = G.UIT.T, config = {text = "Hover to vew more info", align = "tm", colour = G.C.GREY, scale = .4, minw = 2, minh = 6, padding = 0.4, tooltip = {text = {
-									"Prevents jokers added by Fool's Gambit ",
-									"that don't have a vanilla counterpart",
-									"from spawning",
-									" ",
-									"It is not recommended to toggle",
-									"this option during a run",
-									"{C:dark_edition}This option is experimental{}"
-									}
-								}}},
-							}},
-						}},
-					}}
+		{n = G.UIT.R, config = {align = "tm"}, nodes = {
+			{n = G.UIT.C, config = {align = "cm"}, nodes = {
+				{n = G.UIT.R, config = {align = "tm"}, nodes = {
+					create_toggle{
+						label = "Additional jokers",
+						ref_table = FG.config,
+						ref_value = "extra_jokers"
+					},
+				}},
+			}},
+			{n = G.UIT.C, config = {align = "cm"}, nodes = {
+				{n = G.UIT.R, config = {align = "cm"}, nodes = {
+					{n = G.UIT.T, config = {text = "(?)", colour = G.C.GREY, scale = .4, tooltip = {text = {
+						"Prevents jokers added by Fool's Gambit ",
+						"that don't have a vanilla counterpart",
+						"from spawning",
+						" ",
+						"It is not recommended to toggle",
+						"this option during a run",
+						"{C:dark_edition}This option is experimental{}"
+						}
+					}}},
+				}},
+			}},
+			{n = G.UIT.C, config = {align = "cm", minw = 0.3}}, -- separator
+			{n = G.UIT.C, config = {align = "cm"}, nodes = {
+				{n = G.UIT.R, config = {align = "cm"}, nodes = {
+					create_toggle{
+						label = "Duplicated jokers",
+						ref_table = FG.config,
+						ref_value = "duplicated_jokers"
+					},
+					
+				}},
+			}},
+			{n = G.UIT.C, config = {align = "cm"}, nodes = {
+				{n = G.UIT.R, config = {align = "cm"}, nodes = {
+					{n = G.UIT.T, config = {text = "(?)", colour = G.C.GREY, scale = .4, tooltip = {text = {
+						"Allows jokers to spawn even if",
+						"it's alternate is in your joker tray",
+						" ",
+						"It is not recommended to toggle",
+						"this option during a run",
+						"{C:dark_edition}This option is experimental{}"
+						}
+					}}},
+				}},
+			}}
+		}},
+		{n = G.UIT.R, config = {align = "cm"}, nodes = {
+			{n = G.UIT.C, config = {align = "cm"}, nodes = {
+				{n = G.UIT.R, config = {align = "cm"}, nodes = {
+					create_toggle{
+						label = "Alternate SFX",
+						ref_table = FG.config,
+						ref_value = "alternate_sfx",
+					},
+					
 				}},
 				
 			}},
-			{n = G.UIT.R, nodes = {
-				{n = G.UIT.C, nodes = {
-					{n = G.UIT.R, nodes = {
-						{n = G.UIT.C, nodes = {
-							create_toggle{
-								label = "Disable duplicated Jokers",
-								ref_table = FG.config,
-								ref_value = "duplicated_jokers"
-							},
-							{n = G.UIT.R, config = {align = "tm"}, nodes = {
-								{n = G.UIT.T, config = {text = "Hover to vew more info", align = "tm", colour = G.C.GREY, scale = .4, minw = 2, minh = 6, padding = 0.4, tooltip = {text = {
-									"Prevents original and alternate jokers",
-									"from spawning when the counterpart is",
-									"in possesion.",
-									"{C:inactive}(Affected by Showman){}",
-									"{C:dark_edition}This option is experimental{}"
-									}
-								}}},
-							}},
-						}},
-					}},
+			{n = G.UIT.C, config = {align = "cm"}, nodes = {
+				{n = G.UIT.R, config = {align = "cm"}, nodes = {
+					{n = G.UIT.T, config = {text = "(?)", colour = G.C.GREY, scale = .4, tooltip = {text = {
+							"Replaces balatro's sound effects",
+							"with custom ones",
+							" ",
+							"{C:red}Requires restart{}"
+						}
+					}}},
 				}},
 			}},
-			{n = G.UIT.R, config = {minh = 0.3}},
-			{n = G.UIT.R, config = {align = "tm"}, nodes = {{n = G.UIT.T, config = {text = "Special edition", colour = G.C.WHITE, scale = .4}}}},
-			create_option_cycle{
-				options = FG.config.s_version.options,
-				current_option = FG.config.s_version.selected,
-				opt_callback = 'FG_s_version',
-				ref_table = FG.config.s_version,
-				ref_value = "selected",
-                w = 4
-			},
-			{n = G.UIT.R, config = {align = "tm"}, nodes = {{n = G.UIT.T, config = {text = "(Requires restart)", colour = G.C.GREY, scale = .3}}}},
-			{n = G.UIT.R, nodes = {
-				{n = G.UIT.C, nodes = {
+		}},
+		{n = G.UIT.R, config = {align = "cm"}, nodes = {
+			{n = G.UIT.C, config = {align = "cm"}, nodes = {
+				{n = G.UIT.R, config = {align = "cm"}, nodes = {
+					{n = G.UIT.R, config = {align = "cm"}, nodes = {
+						{n = G.UIT.T, config = {text = "Special edition", colour = G.C.WHITE, scale = .4, minw = 2, minh = 6, padding = 0.4}},
+					}},
+					{n = G.UIT.R, config = {align = "cm", minj = 0.2}}, -- separator
+					create_option_cycle{
+						options = FG.config.s_version.options,
+						current_option = FG.config.s_version.selected,
+						opt_callback = 'FG_s_version',
+						ref_table = FG.config.s_version,
+						ref_value = "selected",
+						w = 4
+					},
+				}}
+			}}
+		}},
+		{n = G.UIT.R, config = {align = "cm"}, nodes = {
+			{n = G.UIT.C, config = {align = "cm"}, nodes = {
+				{n = G.UIT.R, config = {align = "cm"}, nodes = {
 					create_toggle{
-						label = "Debug mode",
+						label = "Enable debug mode",
 						ref_table = FG.config,
 						ref_value = "debug_mode"
 					},
-					{n = G.UIT.R, config = {align = "tm"}, nodes = {
-						{n = G.UIT.T, config = {text = "(Requires restart)", align = "tm", colour = G.C.GREY, scale = .3, minw = 2, minh = 6, padding = 0.4}},
-					}},
-					{n = G.UIT.R, config = {align = "tm"}, nodes = {
-						{n = G.UIT.T, config = {text = "\"It is called debug for a reason, you hear me?\"", align = "tm", colour = G.C.GREY, scale = .2, minw = 2, minh = 6, padding = 0.4}},
-					}},
 				}},
-			}},
-		}}
+				{n = G.UIT.R, config = {align = "cm"}, nodes = {
+					{n = G.UIT.T, config = {text = "(Requires restart)", colour = G.C.GREY, scale = .4, minw = 2, minh = 6, padding = 0.4}},
+				}}
+			}}
+		}},
 	}}
 end
 
--- content/misc.lua
+++ content/misc.lua
@@ -31,3 +31,96 @@ SMODS.Stake{
         
     end
 }
+
+
+
+--- 
+--- Sound replacer
+--- 
+if FG.config.alternate_sfx then
+    FG.sounds = {
+        "ambientFire1",
+        "ambientFire2",
+        "ambientFire3",
+        "ambientOrgan1",
+        "button",
+        "cancel",
+        "card1",
+        "card3",
+        "cardFan2",
+        "cardSlide1",
+        "cardSlide2",
+        "chips1",
+        "chips2",
+        "coin1",
+        "coin2",
+        "coin3",
+        "coin4",
+        "coin5",
+        "coin6",
+        "coin7",
+        "crumple1",
+        "crumple2",
+        "crumple3",
+        "crumple4",
+        "crumple5",
+        "crumpleLong1",
+        "crumpleLong2",
+        "explosion_buildup1",
+        "explosion_release1",
+        "explosion1",
+        "foil1",
+        "foil2",
+        "generic1",
+        "glass1",
+        "glass2",
+        "glass3",
+        "glass4",
+        "glass5",
+        "glass6",
+        "gold_seal",
+        "gong",
+        "highlight1",
+        "highlight2",
+        "holo1",
+        "introPad1",
+        "magic_crumple",
+        "magic_crumple2",
+        "magic_crumple3",
+        "multihit1",
+        "multihit2",
+        "negative",
+        "other1",
+        "paper1",
+        "polychrome1",
+        "slice1",
+        "splash_buildup",
+        "tarot1",
+        "tarot2",
+        "timpani",
+        "voice1",
+        "voice2",
+        "voice3",
+        "voice4",
+        "voice5",
+        "voice6",
+        "voice7",
+        "voice8",
+        "voice9",
+        "voice10",
+        "voice11",
+        "whoosh_long",
+        "whoosh",
+        "whoosh1",
+        "whoosh2",
+        "win"
+    }
+    for _,sound in ipairs(FG.sounds) do
+        print(sound)
+        SMODS.Sound{
+            key = sound,
+            path = sound..".ogg",
+            replace = sound
+        }
+    end
+end
